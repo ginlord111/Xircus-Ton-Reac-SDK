c70766d2728d8f1b17e27d4f65b5dec01025f892
ginlord <nicandro.ronnie@gmail.com>
2024-04-15 17:13:48 +0800

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
-
+import { XircusTonProvider } from '@xircus-web3/ton-react'
+import { ChakraProvider } from '@chakra-ui/react'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <XircusTonProvider> 
+    <ChakraProvider >
+  <Component {...pageProps}  />
+  </ChakraProvider>
+  </XircusTonProvider>
+  
+  )
+
 }
